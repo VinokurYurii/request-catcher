9e86c3ec96c492cd9380017c9bc569b4175ac64a
Yurii <vinokyr.yuri@gmail.com>
2018-04-13 00:16:31 +0300

    fix web socket config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,8 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "requests-trap_#{Rails.env}"
   config.action_mailer.perform_caching = false
+  config.action_cable.url = "ws://requests-catcher.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = [/http:\/\/*/, /https:\/\/*/]
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
